9c189648c2cc8f79ddfe8d771b55c4416329eb1c
jj <jj@imput.net>
2026-01-04 22:57:57 +0000

    ci: init

---
+++ .github/do-update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euxo pipefail
+
+sudo rm -rf /usr/local/lib/android \
+            /usr/local/.ghcup \
+            /usr/lib/jvm \
+            /usr/lib/google-cloud-sdk \
+            /usr/lib/dotnet \
+            /usr/share/swift &
+
+sudo apt-get -y install git-filter-repo
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+
+./update.sh
+
+git push
