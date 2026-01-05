04635e7b704b3bbda2cfe6562f45f1647dc9139d
jj <jj@imput.net>
2026-01-05 11:05:30 +0000

    ci: use /mnt

--- .github/do-update.sh
+++ .github/do-update.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
+sudo cp -r . /mnt/repo
+cd /mnt/repo
+sudo chown -R $(id -u):$(id -g) .
+
 sudo apt-get -y install git-filter-repo
-sudo rm -rf /usr/local/lib/android \
-            /usr/local/.ghcup \
-            /usr/lib/jvm \
-            /usr/lib/google-cloud-sdk \
-            /usr/lib/dotnet \
-            /usr/lib/firefox \
-            /opt/az \
-            /opt/ghc \
-            /opt/google \
-            /opt/hostedtoolcache \
-            /opt/microsoft \
-            /usr/share
 
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
